1548599f83269f7fe3729d0138d2511b2ba7bdbf
ehrud3476 <74747311+ehrud3476@users.noreply.github.com>
2021-04-18 23:31:13 +0900

    21.04.18

---
+++ 21.04.18/DQNode/src/DQ_Test.java
@@ -0,0 +1,37 @@
+
+ class DQ_Test {
+
+	public static void main(String[] args) {
+			char deletedItem;
+			DQueue DQ = new DQueue();
+			
+			DQ.insertFront('A');
+			DQ.printDQueue();
+			
+			DQ.insertFront('B');
+			DQ.printDQueue();
+			
+			DQ.insertRear('C');
+			DQ.printDQueue();
+			
+			deletedItem = DQ.deleteFront();
+			if(deletedItem != 0)
+				System.out.println("Front deleted Item : " + deletedItem);
+			DQ.printDQueue();
+			
+			deletedItem = DQ.deleteRear();
+			if(deletedItem != 0)
+				System.out.println("Rear  deleted Item : " + deletedItem);
+			DQ.printDQueue();
+			
+			DQ.insertRear('D');
+			DQ.printDQueue();
+			
+			DQ.insertFront('E');
+			DQ.printDQueue();
+
+			DQ.insertFront('F');
+			DQ.printDQueue();
+	}
+
+}
